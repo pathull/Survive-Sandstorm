00b7e902902fcb729b58165ab020e57a6dfd767a
Patrick Hull <patrick9hull4@gmail.com>
2023-03-30 11:25:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ function checkCollisions() {
 }
 
 // Game loop
-function gameLoop() {
+function gameLoap() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawPlayer();
   checkCollisions();
