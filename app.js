39e4c972b108d6f81c577121bc34aada1f33da77
Patrick Hull <patrick9hull4@gmail.com>
2023-03-30 11:23:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,31 @@ const winningText = document.querySelector("#winning-text");
 const winningButton = document.querySelector("#play-again");
 const audio = document.getElementById("audio");
 
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 //Audio
 function playAudio() {
   audio.play();
@@ -165,7 +190,7 @@ function gameLoop() {
   checkCollisions();
   drawDots();
   updateDots();
-  requestAnimationFrame(gameLoop);
+  window.requestAnimationFrame(gameLoop);
 }
 
 // Start the game loop
